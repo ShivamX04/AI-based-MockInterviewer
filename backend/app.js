11aa873f3d0d955168094c7fb0decc736ee0de46
shivam tiwari <st532288@gmail.com>
2026-07-30 14:40:03 +0530

    Your commit message

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://ai-based-mockinterviewer.onrender.com"
+     ],
     credentials: true,
 }));
 
