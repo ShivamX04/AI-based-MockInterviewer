94d31f7b677c9fc8fe2b4ae6ef822c81f5403e47
shivam tiwari <st532288@gmail.com>
2026-08-25 16:25:26 +0530

    Fix authentication cookies for production

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -95,11 +95,11 @@ export const loginUserController = async (req, res) => {
         );
 
         res.cookie("token", token, {
-            httpOnly: true,
-            secure: false, // change to true in production HTTPS
-            sameSite: "lax",
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-        });
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
         return res.status(200).json({
             success: true,
